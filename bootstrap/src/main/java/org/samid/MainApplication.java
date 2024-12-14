74d1e4ded119131cb04c43ac9db5687798ed6892
samidarslan <samidarslan@gmail.com>
2024-12-14 14:10:39 +0530

    added swagger annotation

--- bootstrap/src/main/java/org/samid/MainApplication.java
+++ bootstrap/src/main/java/org/samid/MainApplication.java
@@ -1,9 +1,21 @@
 package org.samid;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Student Management API",
+                version = "1.0"
+        ),
+        externalDocs = @ExternalDocumentation(
+                url = "https://github.com/SamidArsalan/student-management-service/blob/main/README.md"
+        )
+)
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
